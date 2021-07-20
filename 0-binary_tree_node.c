f92d38787be7d44e41c159d539f017fe44674370
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-19 19:13:57 -0500

    Uploading task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/* binary_tree_t - function that creates a binary tree node
+/* binary_tree_node - function that creates a binary tree node
  *
  * @parent: pointer to the parent node of the node to create
  * @value: the value to put in the new node
