532e9d1e8f159c914978535281af503721cf0ba1
dianaparr <dianaparra.9611@gmail.com>
2021-07-19 23:35:47 -0500

    Valgrind fixes and modification of insert node left and right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,25 +15,32 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
+	// new_node = malloc(sizeof(binary_tree_t));
+	// if (new_node == NULL)
+	// {
+	// 	return (NULL);
+	// }
 
-	new_node->n = value;
-	if (parent->right == NULL)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	// new_node->n = value;
+	// if (parent->right == NULL)
+	// {
+	// 	parent->right = new_node;
+	// }
+	// else
+	// {
+	// 	parent->right->parent = new_node;
+	// 	new_node->right = parent->right;
+	// }
+	if (parent->right)
 	{
-		parent->right = new_node;
-		new_node->parent = parent;
-	}
-	else
-	{
+		parent->right->parent = new_node;
 		new_node->right = parent->right;
-		parent->right = new_node;
-		new_node->parent = parent;
-		new_node->right->parent = new_node;
 	}
+	parent->right = new_node;
 
 	return (new_node);
 }
