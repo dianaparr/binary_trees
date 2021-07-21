851885b4f592306109dd035471e6e40cdb7ef159
dianaparr <dianaparra.9611@gmail.com>
2021-07-21 09:47:27 -0500

    Check the betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,26 +15,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	// new_node = malloc(sizeof(binary_tree_t));
-	// if (new_node == NULL)
-	// {
-	// 	return (NULL);
-	// }
-
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	// new_node->n = value;
-	// if (parent->left == NULL)
-	// {
-	// 	parent->left = new_node;
-	// }
-	// else
-	// {
-	// 	parent->left->parent = new_node;
-	// 	new_node->left = parent->left;
-	// }
 	if (parent->left)
 	{
 		parent->left->parent = new_node;
