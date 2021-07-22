1167469a697b507cacc8a28cb105b67aa1d6b206
Mateo Londoño <mateolondono.u@gmail.com>
2021-07-22 10:17:45 -0500

    Updating hight function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,12 +46,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left)
 		left_height = binary_tree_height(tree->left) + 1;
 	else
-		left_height = 0;
+		left_height = 1;
 
 	if (tree->right)
 		right_height = binary_tree_height(tree->right) + 1;
 	else
-		right_height = 0;
+		right_height = 1;
 
 	if (left_height > right_height)
 		return (left_height);
