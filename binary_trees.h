9561bdc41b71c342a332abdaeb9212ee040d99d2
dianaparr <dianaparra.9611@gmail.com>
2021-07-26 19:57:20 -0500

    Updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,6 @@ int _pow_recursion(int x, int y);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void print_nodes_level_current(const binary_tree_t *tree, void (*func)(int), int level);
-
+int binary_tree_is_complete(const binary_tree_t *tree);
 
 #endif
